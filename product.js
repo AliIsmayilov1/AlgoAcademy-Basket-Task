9062d2878fcb5b6c39327e9201c899415a51b222
aliismayilov <t232986@gmail.com>
2024-04-14 12:19:19 +0400

    Remove from basket function added,basket fixed

--- product.js
+++ product.js
@@ -1,6 +1,11 @@
 function getProduct() {
     return JSON.parse(localStorage.getItem("product") || "[]")
-
+}
+function getBasket() {
+    return JSON.parse(localStorage.getItem("basket") || "[]")
+}
+function setBasket(arr) {
+    localStorage.setItem("basket", JSON.stringify(arr))
 }
 
 function generateProduct() {
@@ -16,44 +21,62 @@ function generateProduct() {
         </div>
         `
         document.getElementById("main").innerHTML = x
-        document.getElementById("bskt").style.display="block"
-        document.getElementById("menu").style.display="none"
     }
 }
-let myBasket = []
 function addToBasket(x) {
     let fl = 1
-    for (let i = 0; i < myBasket.length; i++) {
-        if (myBasket[i] == x) {
+    let basket = getBasket()
+    for (let i = 0; i < basket.length; i++) {
+        if (basket[i] == x) {
             fl = 0
+            break;
         }
     }
     if (fl == 1) {
-        myBasket.push(x)
+        basket.push(x)
+        setBasket(basket)
     }
 }
 function basket() {
     let x = ""
     let prd = getProduct()
     let totalPrice = 0
-    for (let i = 0; i < myBasket.length; i++) {
+    let basket = getBasket()
+    for (let i = 0; i < prd.length; i++) {
+        if(basket.includes(prd[i].id)){
         x += `
-        <div id="${prd[myBasket[i]].id}">
-        <img src="${prd[myBasket[i]].picture}" alt="">
-        <p id="name">${prd[myBasket[i] - 1].name}</p>
-        <p id="price">${prd[myBasket[i] - 1].price}</p>
-            <p id="${prd[myBasket[i] - 1].id}" class="btn" onclick="addToBasket(${prd[i].id})" >Add to basket</p>
+        <div id="${prd[i].id}">
+        <img src="${prd[i].picture}" alt="">
+        <p id="name">${prd[i].name}</p>
+        <p id="price">${prd[i].price}</p>
+            <p id="${prd[i].id}" class="btn" onclick="addToBasket(${prd[i].id})" >Add to basket</p>
+            <i onclick="removeFromBasket(${prd[i].id})" title="Remove from basket" class="fa-solid fa-xmark"></i>
             </div>
             `
         document.getElementById("main").innerHTML = x;
-        totalPrice += +prd[myBasket[i] - 1].price
+        totalPrice += +prd[i].price
+        }
     }
-    x += `<h1>Total Price: ${totalPrice}</h1>`
+    x += `<h1>Total Price: ${totalPrice}$</h1>`
     document.getElementById("main").innerHTML = x;
-        
-    console.log(myBasket);
-    console.log(totalPrice);
-    document.getElementById("bskt").style.display="none"
-    document.getElementById("menu").style.display="block"
+    document.getElementById("bskt").style.display = "none"
+    document.getElementById("menu").style.display = "block"
+    setBasket(basket)
+}
+function backToMenu() {
+    generateProduct()
+    document.getElementById("bskt").style.display = "block"
+    document.getElementById("menu").style.display = "none"
+}
+function removeFromBasket(x) {
+    let oldBasket = getBasket()
+    let newBasket = []
+    for(let i = 0;i<oldBasket.length;i++){
+        if(oldBasket[i] != x){
+            newBasket.push(oldBasket[i])
+        }
+    }
+    setBasket(newBasket)
+    basket()
 }
 generateProduct()
